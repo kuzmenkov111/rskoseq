f7d3aa02e6609aea2501327ac21585108b456523
shkonishi <shkonishi@gmail.com>
2018-05-15 09:35:19 +0900

    180515

--- R/rawfqext.R
+++ R/rawfqext.R
@@ -1,73 +1,92 @@
 #' rawfqext
 #' @description merge NEXTSEQ output fastq, which created by BaseSpace
-#' @usage rawfqext(rawfq_dir, prefix)
+#' @usage rawfqext(rawfq_dir, paired, prefix)
 #' @param rawfq_dir fastq containing directory
-#' @param prefix sample name
+#' @param paired logical, if TRUE is paired end read.
+#' @param prefix Characters of sample name, The default value is NULL, collect from directories name.
 #' @examples
 #' # rawfq_dir <- "~/pub/sampledata/fastq/NEXTSEQ_OUT"
 #' # prefix <- c("S1","S2")
 #' # rawfqext(rawfq_dir, prefix)
 #' @export
 
-rawfqext <- function(rawfq_dir, prefix){
+rawfqext <- function(rawfq_dir, paired = FALSE, prefix = NULL){
 
   # collect fastq containing directory -----
   dirs <- list.files(rawfq_dir,  full.names = T)
 
-  # argument check:
-  if (length(dirs) != length(prefix)){
+  # argument check: prefix----
+  if (is.null(prefix)){
+    prefix <- sapply(strsplit(list.files(rawfq_dir), "\\-"), "[", 1)
+
+  } else if (length(dirs) != length(prefix)){
     stop("length of fasq directory and prefix must to be same")
+
   }
 
+  # merge files of read per lane -----
   for (i in seq_along(dirs)){
-    # collect fasq file path ----
-    gzr1 <- list.files(dirs[i], ".*_R1_.*.fastq.gz$", full.names = T)
-    gzr2 <- list.files(dirs[i], ".*_R2_.*.fastq.gz$", full.names = T)
-    r1 <- list.files(dirs[i], ".*_R1_.*.fastq$", full.names = T)
-    r2 <- list.files(dirs[i], ".*_R2_.*.fastq$", full.names = T)
+    if (paired == T){
+      gzr1 <- list.files(dirs[i], ".*_R1_.*.fastq.gz$", full.names = T)
+      gzr2 <- list.files(dirs[i], ".*_R2_.*.fastq.gz$", full.names = T)
+      r1 <- list.files(dirs[i], ".*_R1_.*.fastq$", full.names = T)
+      r2 <- list.files(dirs[i], ".*_R2_.*.fastq$", full.names = T)
 
-    # still uncompress ----
-    if (identical(gzr1, character(0)) ){
-      if (!identical(r1, character(0))){ # R1 uncompressed fastq
-        com1 <- paste0("cat ",
-                       paste(r1, collapse = " "),
-                       " | gzip -c > ",
-                       rawfq_dir, "/",
-                       prefix[i], "_R1.fastq.gz")
+      if (!identical(gzr1, character(0)) & identical(r1, character(0))){
+        # command of merge R1 & R2 compressed files ----
+        com1g <- paste("cat", paste(gzr1, collapse = " "), ">",
+                       paste0(rawfq_dir, "/", prefix[i], "_R1.fastq.gz")
+        )
+        com2g <- paste("cat", paste(gzr2, collapse = " "), ">",
+                       paste0(rawfq_dir, "/", prefix[i], "_R2.fastq.gz")
+        )
+        # execute command -----
+        system(com1g)
+        cat(paste0(com1g, "\n"))
+        system(com2g)
+        cat(paste0(com2g, "\n"))
+
+      } else if (identical(gzr1, character(0)) & !identical(r1, character(0))){
+        # uncompressed R1 & R2 files are merged and compressd to gz file ----
+        com1 <- paste("cat", paste(r1, collapse = " "),
+                      "| gzip -c >",
+                      paste0(rawfq_dir, "/", prefix[i], "_R1.fastq.gz")
+        )
+        com2 <- paste("cat", paste(r2, collapse = " "),
+                      "| gzip -c >",
+                      paste0(rawfq_dir, "/", prefix[i], "_R2.fastq.gz")
+        )
         system(com1)
-
-      } else if (!identical(r2, character(0))){ # R2 uncompressed fastq
-        com2 <- paste0("cat ",
-                       paste(r2, collapse = " "),
-                       " | gzip -c > ",
-                       rawfq_dir, "/",
-                       prefix[i], "_R2.fastq.gz")
         system(com2)
 
       }
 
-    # compressed fastq gz to merged gz ----
-    } else if (!identical(gzr1, character(0))){
-      if (!identical(gzr1, character(0))){ # R1
+    } else if (paired == F){
+      gzr1 <- list.files(dirs[i], ".*_R1_.*.fastq.gz$", full.names = T)
+      r1 <- list.files(dirs[i], ".*_R1_.*.fastq$", full.names = T)
 
-        com1g <- paste0("cat ",
-                        paste(gzr1, collapse = " "),
-                        " > ",
-                        rawfq_dir, "/",
-                        prefix[i], "_R1.fastq.gz")
+      if (!identical(gzr1, character(0)) & identical(r1, character(0))){
+        # command of merge R1 & R2 compressed files ----
+        com1g <- paste("cat", paste(gzr1, collapse = " "), ">",
+                       paste0(rawfq_dir, "/", prefix[i], "_R1.fastq.gz")
+        )
+        # execute command -----
         system(com1g)
-        cat(com1g)
+        cat(paste0(com1g, "\n"))
 
-      } else if (!identical(gzr2, character(0))){ # R2
-        com2g <- paste0("cat ",
-                        paste(gzr2, collapse = " "),
-                        " > ",
-                        rawfq_dir, "/",
-                        prefix[i], "_R2.fastq.gz")
-        system(com2g)
-        cat(paste0(com2g, "\n"))
+      } else if (identical(gzr1, character(0)) & !identical(r1, character(0))){
+        # uncompressed R1 & R2 files are merged and compressd to gz file ----
+        com1 <- paste("cat", paste(r1, collapse = " "),
+                      "| gzip -c >",
+                      paste0(rawfq_dir, "/", prefix[i], "_R1.fastq.gz")
+        )
+        system(com1)
+        cat(paste0(com1, "\n"))
+
       }
+
     }
+
   }
 }
 
